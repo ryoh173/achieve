9baa4e3b94d99a94fe722810cbe6e9ab53903c8b
Ryo Hasebe <wtc.r.h173@gmail.com>
2017-05-13 10:01:32 +0000

    first commit

---
+++ app/controllers/contacts_controller.rb
@@ -0,0 +1,30 @@
+class ContactsController < ApplicationController
+  before_action :contact_params, only: [:create]
+  
+  def new
+    if params[:back]
+    @contact = Contact.new(contact_params)
+    else
+    @contact = Contact.new
+    end
+  end
+
+  def create
+    @contact = Contact.new(contact_params)
+    if @contact.save
+      redirect_to new_contact_path, notice: "お問い合わせが完了しました！"
+    else
+      render "new"
+    end
+  end
+
+  def confirm
+    @contact = Contact.new(contact_params)
+    render :new if @contact.invalid?
+  end
+  
+  private
+    def contact_params
+      params.require(:contact).permit(:name, :email, :content)
+    end
+end
